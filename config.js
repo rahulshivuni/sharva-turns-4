38e0f3820264ee8708aebd790791dfa48109d842
RahulShivuni <rahul.shivuni@gmail.com>
2026-09-13 18:35:35 -0500

    update location

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ window.PARTY = {
   tagline: "Join us for a Superhero Birthday Bash!",
   celebrate: "OUR LITTLE SUPERHERO",
   date: "September 19, 2026",
-  time: "6:00 PM",
-  place: "St. Louis, MO",
+  time: "5:00 PM",
+  place: "Go Play!, Location: 21 Meadow Cir Dr #308, Lake St Louis, MO 63367",
   hostEmail: "rahul.shivuni@gmail.com",
   // Create a free pantry at https://getpantry.cloud and paste the ID here
   // so every friend who opens the GitHub Pages link shares one guest list.
